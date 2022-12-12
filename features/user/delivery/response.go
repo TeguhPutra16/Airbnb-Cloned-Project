5bb8330ec4efa967ea55af450ec44f85551675d8
Anti-Gen <putrategu@gmail.com>
2022-12-12 21:24:19 +0700

    rev-response-user

--- features/user/delivery/response.go
+++ features/user/delivery/response.go
@@ -6,7 +6,6 @@ type UserRespon struct {
 	ID      uint   `json:"id"`
 	Name    string `json:"name"`
 	Email   string `json:"email"`
-	Phone   string `json:"phone"`
 	Role    string `json:"role" form:"role"`
 	Address string `json:"address"`
 }
